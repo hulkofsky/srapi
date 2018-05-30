470ac3479061f1ff572c09029711a4c7e3aec5b8
akolosay <andrej.kolosay@gmail.com>
2018-05-30 11:13:33 +0300

    FG-52

--- front/.next/static/commons/manifest.js
+++ front/.next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "c9c41fae8b38a8af75d3"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "b74350dd5729be8df643"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_app.js","2":"bundles\\pages\\_error.js","4":"bundles\\pages\\index.js","5":"bundles\\pages\\stance.js"}[chunkId]||chunkId) + "-" + {"0":"e5162e79a870eef4363b","1":"713744e63b985006df7c","2":"f70ecd4a32353853c9cf","4":"f57d669b5d323a5bf09c","5":"421500cb51822c33fce8"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_app.js","2":"bundles\\pages\\_error.js","4":"bundles\\pages\\index.js"}[chunkId]||chunkId) + "-" + {"0":"e5162e79a870eef4363b","1":"713744e63b985006df7c","2":"f70ecd4a32353853c9cf","4":"f57d669b5d323a5bf09c"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
