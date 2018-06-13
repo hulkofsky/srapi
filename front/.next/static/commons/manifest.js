a995a1134a83b603c775183d562938d172b858fc
akolosay <andrej.kolosay@gmail.com>
2018-06-13 15:12:28 +0300

    https://vitaminlondon.atlassian.net/browse/FG-96

--- front/.next/static/commons/manifest.js
+++ front/.next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "1309ec89e9e5090e0528"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "3e8b32b0767bfe401a12"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_app.js","2":"bundles\\pages\\_error.js","4":"bundles\\pages\\index.js","6":"bundles\\pages\\location.js"}[chunkId]||chunkId) + "-" + {"0":"e5162e79a870eef4363b","1":"a5d81f43661c1ba9e33f","2":"f70ecd4a32353853c9cf","4":"b9a8571b7f580c35aa46","6":"ed2d618ca80cfa7ca4d7"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_app.js","2":"bundles\\pages\\_error.js","4":"bundles\\pages\\index.js"}[chunkId]||chunkId) + "-" + {"0":"e5162e79a870eef4363b","1":"713744e63b985006df7c","2":"f70ecd4a32353853c9cf","4":"1da0d5ea36e058194ce6"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
